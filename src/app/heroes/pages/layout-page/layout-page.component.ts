0a56337b553e99adb3bf726966aeb7f1a6ae1e52
Victor Rodriguez Garcia <vrodriguez@opentrends.net>
2024-07-27 11:22:14 +0200

    fake backend

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class LayoutPageComponent {
-
+  sidebarItems = [
+    { label: 'Add', icon: 'add', url: './new-hero' },
+    { label: 'List', icon: 'label', url: './list' },
+    { label: 'Search', icon: 'search', url: './search' },
+  ];
 }
